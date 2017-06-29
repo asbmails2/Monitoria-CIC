ab9d388908bff988c5c8b993af6d6f84427c72f4
asbmails2 <asbmails2@gmail.com>
2017-06-28 23:49:04 -0300

    Adicionando as colunas do tipo de usuário em cada usuário.

--- db/migrate/20170629023755_add_roles_to_users.rb
+++ db/migrate/20170629023755_add_roles_to_users.rb
@@ -1,7 +1,7 @@
 class AddRolesToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :admin_role, :boolean
-    add_column :users, :professor_role, :boolean
-    add_column :users, :student_role, :boolean
+    add_column :users, :admin_role, :boolean, default: false
+    add_column :users, :professor_role, :boolean, default: false
+    add_column :users, :student_role, :boolean, default: true
   end
 end
