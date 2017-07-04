f364547b5f20f0daceb701da4497d51fccffb599
asbmails2 <asbmails2@gmail.com>
2017-07-03 22:49:22 -0300

    ajuste migrates - redução de linhas inúteis

--- db/migrate/20161101132911_criar_professor.rb
+++ db/migrate/20161101132911_criar_professor.rb
@@ -5,7 +5,6 @@ class CriarProfessor < ActiveRecord::Migration[5.0]
       t.string 'email'
       t.string 'sala'
       t.timestamps #Manter rastro de criação/modificação
-      t.integer 'id' #relacionar com o usuário.
     end
   end
 end
--- db/migrate/20161210121345_create_oferta.rb
+++ db/migrate/20161210121345_create_oferta.rb
@@ -3,7 +3,6 @@ class CreateOferta < ActiveRecord::Migration[5.0]
     create_table :oferta do |t|
       t.string :nome
       t.string :codigo
-
       t.timestamps
     end
   end
